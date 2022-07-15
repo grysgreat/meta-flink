3e9557a04b7674d6f1b28c0600e5003caaf7cfb9
Luxian <xiaoyang@isrc.iscas.ac.cn>
2022-07-15 21:43:52 +0800

    [feat]: 添加用户job插入接口

--- src/main/java/com/example/metaflink/controller/UserJobConfigController.java
+++ src/main/java/com/example/metaflink/controller/UserJobConfigController.java
@@ -2,8 +2,10 @@ package com.example.metaflink.controller;
 
 import com.example.metaflink.Service.UserConfigService;
 import com.example.metaflink.database.config.UserConfig;
+import com.example.metaflink.database.config.UserJobConfig;
 import com.example.metaflink.mapper.UserJobConfigMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.batch.BatchProperties;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
@@ -21,5 +23,14 @@ public class UserJobConfigController {
     public UserConfig UserConfigLogin (HttpServletRequest request)
     {
         return null;
+    }
+
+    @RequestMapping(value = "addjobid" )
+    public boolean AddUserJobid(@RequestParam(value ="jobid",required = true)String Jobid,@RequestParam(value ="userid",required = true)int userid){
+        UserJobConfig userJobConfig = new UserJobConfig();
+        userJobConfig.setJobId(Jobid);
+        userJobConfig.setUserId(userid);
+        this.userJobConfigMapper.InsertJob(userJobConfig);
+        return true;
     }
 }
